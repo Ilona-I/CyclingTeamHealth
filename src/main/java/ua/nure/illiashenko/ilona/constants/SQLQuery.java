f9642805319bd81861031817d4b8f2aaee4903ba
ilona illiashenko <ilona.illiashenko@gmail.com>
2022-05-21 22:58:41 +0200

    Add UpdateTrainingGoalServlet

--- src/main/java/ua/nure/illiashenko/ilona/constants/SQLQuery.java
+++ src/main/java/ua/nure/illiashenko/ilona/constants/SQLQuery.java
@@ -2,7 +2,45 @@ package ua.nure.illiashenko.ilona.constants;
 
 public class SQLQuery {
 
+    public static final String INSERT_USER = "INSERT INTO `user` (`login`, `password`, `first_name`, `last_name`, `email`, `birth_date`, `height`, `weight`, `gender`, `status`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+    public static final String UPDATE_USER = "";
+    public static final String DELETE_USER = "";
+    public static final String GET_USER_BY_LOGIN = "";
 
-    private SQLQuery(){
+    public static final String INSERT_TEAM = "INSERT INTO `team` (`name`) VALUES (?)";
+    public static final String UPDATE_TEAM = "";
+    public static final String DELETE_TEAM = "";
+    public static final String GET_TEAM = "";
+
+    public static final String ADD_USER_TO_THE_TEAM = "";
+    public static final String DELETE_USER_FROM_THE_TEAM = "";
+    public static final String GET_TEAM_MEMBERS = "";
+    public static final String GET_USER_TEAMS = "";
+
+    public static final String INSERT_CHAT = "";
+    public static final String GET_CHAT = "";
+
+    public static final String INSERT_USER_CHAT = "";
+
+    public static final String INSERT_MESSAGE = "";
+    public static final String GET_MESSAGES_BY_CHAT_ID = "";
+
+    public static final String INSERT_FEEDBACK = "";
+    public static final String UPDATE_FEEDBACK = "";
+    public static final String DELETE_FEEDBACK = "";
+    public static final String GET_FEEDBACKS = "";
+    public static final String GET_FEEDBACK = "";
+
+    public static final String INSERT_TRAINING_GOAL = "";
+    public static final String UPDATE_TRAINING_GOAL = "";
+    public static final String DELETE_TRAINING_GOAL = "";
+    public static final String GET_TRAINING_GOAL = "";
+    public static final String GET_TRAINING_GOALS = "";
+
+    public static final String INSERT_TRAINING_RESULT = "";
+    public static final String GET_TRAINING_RESULT = "";
+    public static final String GET_TRAINING_RESULTS = "";
+
+    private SQLQuery() {
     }
 }
---
+++ src/main/java/ua/nure/illiashenko/ilona/controllers/servlets/training/UpdateTrainingGoalServlet.java
@@ -0,0 +1,33 @@
+package ua.nure.illiashenko.ilona.controllers.servlets.training;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ua.nure.illiashenko.ilona.services.DataValidator;
+import ua.nure.illiashenko.ilona.services.TrainingService;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static ua.nure.illiashenko.ilona.constants.ContextConstants.DATA_VALIDATOR;
+import static ua.nure.illiashenko.ilona.constants.ContextConstants.TRAINING_SERVICE;
+
+@WebServlet("/training/goal")
+public class UpdateTrainingGoalServlet extends HttpServlet {
+
+    private static final Logger logger = LoggerFactory.getLogger(UpdateTrainingGoalServlet.class);
+    private TrainingService trainingService;
+    private DataValidator dataValidator;
+
+    @Override
+    public void init() {
+        trainingService = (TrainingService) getServletContext().getAttribute(TRAINING_SERVICE);
+        dataValidator = (DataValidator) getServletContext().getAttribute(DATA_VALIDATOR);
+    }
+
+    @Override
+    public void doPut(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+}
